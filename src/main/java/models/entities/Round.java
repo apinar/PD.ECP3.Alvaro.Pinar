d1a85a153eb576f1a31da5b0e9f7371f75b4c375
Alvaro <alvaro@apinar>
2014-10-20 22:45:31 +0200

    Clases Round y User.

---
+++ src/main/java/models/entities/Round.java
@@ -0,0 +1,8 @@
+package models.entities;
+
+public class Round {
+	private int id;
+	private int points;
+	private String tablero;
+
+}
---
+++ src/main/java/models/entities/User.java
@@ -0,0 +1,10 @@
+package models.entities;
+
+public class User {
+	private int id;
+	private String name;
+	private String password;
+	private boolean isAdmin;
+	private String description;
+
+}
